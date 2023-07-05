2f80ef4adc48fba61001157564899161528e4d1e
Юрий Сундуков <sundukovyury@yandex.ru>
2023-07-05 15:30:38 +0300

    fix 1.0

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -116,7 +116,7 @@ STATIC_URL = '/static/'
 # в контейнере это будет /app/collected_static
 STATIC_ROOT = BASE_DIR / 'collected_static'
 # Теперь при вызове команды python manage.py collectstatic
-# Django будет копировать все статические файлы в директорию collected_static 
+# Django будет копировать все статические файлы в директорию collected_static
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
